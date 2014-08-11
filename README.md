fd03a2b9e49584dc84e4c18c91c9a8d4d8e7fbf6
Emily Haasch <emhaasch@gmail.com>
2014-08-11 10:42:58 -0500

    Starting from previous code base

---
+++ README.md
@@ -0,0 +1,6 @@
+Tabletop-Deathmatch-2014
+========================
+
+Website for TTDM
+
+~ the repo is for cool people only ~
---
+++ js/global.js
@@ -0,0 +1,9 @@
+// Execute the following code when the page is loaded
+$(function () {
+    // log(), which is just a shortcut for console.log()
+    log = function() {
+        console.log(Array.prototype.slice.call(arguments).join(' '));
+    };
+
+    // Custom JS
+});
